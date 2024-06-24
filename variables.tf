43aa5aeb93f57ad5b5d992330ba3d600c80e73f4
krk2804 <joanna2804@gmail.com>
2024-06-24 14:58:19 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,73 @@
+variable "project_id" {
+  default = "roimb-devopsuser6-dev"
+  type    = string
+}
+
+variable "region" {
+  default = "us-central1"
+  type    = string
+}
+
+variable "topic" {
+  default = "activities"
+  type    = string
+}
+
+variable "function" {
+  default = "pubsubtobq"
+  type    = string
+}
+
+variable "function_sa" {
+  default = "plutosa"
+  type    = string
+}
+
+variable "bucketsuffix" {
+  default = "-bucket"
+  type    = string
+}
+
+variable "asset_list" {
+    type = list(string)
+    default = [
+        "compute.googleapis.com/Instance",
+        "compute.googleapis.com/Image",
+        "compute.googleapis.com/Snapshot",
+        "storage.googleapis.com/Bucket",
+    ]
+}
+
+variable "service_list" {
+    type = list(string)
+    default = [
+      "cloudasset.googleapis.com",
+      "cloudbuild.googleapis.com",
+      "cloudfunctions.googleapis.com",
+      "containerregistry.googleapis.com",
+      "cloudresourcemanager.googleapis.com",
+      "iam.googleapis.com",
+      "pubsub.googleapis.com",
+      "sourcerepo.googleapis.com",
+    ]
+}
+
+variable "default_service_list" {
+    type = list(string)
+    default = [
+      "bigquery.googleapis.com",
+      "bigquerymigration.googleapis.com",
+      "bigquerystorage.googleapis.com",
+      "cloudapis.googleapis.com",
+      "clouddebugger.googleapis.com",
+      "cloudtrace.googleapis.com",
+      "logging.googleapis.com",
+      "monitoring.googleapis.com",
+      "servicemanagement.googleapis.com",
+      "serviceusage.googleapis.com",
+      "sql-component.googleapis.com",
+      "storage.googleapis.com",
+      "storage-api.googleapis.com",
+      "storage-component.googleapis.com"
+    ]
+}
